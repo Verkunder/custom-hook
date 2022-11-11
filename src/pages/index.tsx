926bab43319865a2a96a609837dc741cef76b161
Kapustin Stepan <s.kapustin@webpractik.ru>
2022-11-11 13:34:44 +0300

    feat(size-hook): добавил кастомный хук клика вне элемента

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ImageFetch from '@/components/core/ImageFetch';
 import WindowSizeExampleHook from '@/components/core/WindowSizeExampleHook';
+import Modal from '@/components/core/Modal';
 
 function IndexPage() {
+    const [isModal, setModal] = React.useState(false);
     return (
         <>
             <ImageFetch />
             <WindowSizeExampleHook />
+            <button onClick={() => setModal(true)}>Click Here</button>
+            <Modal
+                isVisible={isModal}
+                title="Modal Title"
+                content={<p>Add your content here</p>}
+                footer={<button>Cancel</button>}
+                onClose={() => setModal(false)}
+            />
         </>
     );
 }
